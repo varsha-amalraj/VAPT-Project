4f3e3917b510d8ef4f93dff825625cd46a733650
Udhayakumar C <udhayaas97@gmail.com>
2020-11-21 19:54:32 +0530

    Meeting updates

--- src/app/pages/customer/customer.module.ts
+++ src/app/pages/customer/customer.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
-import { NbCardModule, NbInputModule } from '@nebular/theme';
-import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { CommonModule } from '@angular/common';
+import { NbCardModule, NbInputModule, NbIconModule, NbDatepickerModule } from '@nebular/theme';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { ReportComponent } from './report/report.component';
 import { CustomerDashboardComponent } from './dashboard/dashboard.component';
 import { CustomerMeetingComponent } from './meeting/meeting.component';
--- src/app/pages/customer/meeting/meeting.component.ts
+++ src/app/pages/customer/meeting/meeting.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ngx-customer-meeting',
   templateUrl: './meeting.component.html',
+  styleUrls: ['./meeting.component.scss'],
 })
 
 export class CustomerMeetingComponent {}
