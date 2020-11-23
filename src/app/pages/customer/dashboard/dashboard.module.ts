1cb53c3bebf89383aa44fd859572db48399c099d
varshaRaj <minivarsha.a@netcon.in>
2020-11-23 11:00:57 +0530

    linting errors

--- src/app/pages/customer/dashboard/dashboard.module.ts
+++ src/app/pages/customer/dashboard/dashboard.module.ts
@@ -7,7 +7,7 @@ import { DashboardComponent } from './dashboard.component';
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+  ],
 })
 export class DashboardModule { }
