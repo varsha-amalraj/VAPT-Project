ecd961e3a3fe005df131135c30a364a02b3f476c
varshaRaj <minivarsha.a@netcon.in>
2020-11-23 13:26:24 +0530

    dashboard page

--- src/app/pages/customer/dashboard/dashboard.component.ts
+++ src/app/pages/customer/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from '../../../../../node_modules/chart.js';
 
 @Component({
   selector: 'ngx-dashboard',
@@ -10,6 +11,24 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var myChart = new Chart("myChart", {
+      type: 'pie',
+      data: {
+        // labels: ["Green", "Blue", "Gray", "Purple", "Yellow", "Red", "Black"],
+        datasets: [{
+          backgroundColor: [
+            "#2ecc71",
+            "#3498db",
+            "#95a5a6",
+            "#9b59b6",
+            "#f1c40f",
+            "#e74c3c",
+            "#34495e"
+          ],
+          data: [12, 19, 3, 17, 28, 24, 7]
+        }]
+      }
+    });
   }
 
 }
