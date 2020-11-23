b37286b9b80e3508fbd3958ad531c200d25e6dee
varshaRaj <minivarsha.a@netcon.in>
2020-11-23 14:34:20 +0530

    dashboard-changes

--- src/app/pages/customer/dashboard/dashboard.component.ts
+++ src/app/pages/customer/dashboard/dashboard.component.ts
@@ -12,19 +12,19 @@ export class CustomerDashboardComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit() {
-    var myChart = new Chart("myChart", {
+    let myChart = new Chart('myChart', {
       type: 'pie',
       data: {
         // labels: ["Green", "Blue", "Gray", "Purple", "Yellow", "Red", "Black"],
         datasets: [{
           backgroundColor: [
-            "#2ecc71",
-            "#3498db",
-            "#95a5a6",
-            "#9b59b6",
-            "#f1c40f",
-            "#e74c3c",
-            "#34495e"
+            '#2ecc71',
+            '#3498db',
+            '#95a5a6',
+            '#9b59b6',
+            '#f1c40f',
+            '#e74c3c',
+            '#34495e',
           ],
           data: [12, 19, 3, 17, 28, 24, 7]
         }]
--- src/app/pages/customer/dashboard/dashboard.module.ts
+++ src/app/pages/customer/dashboard/dashboard.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './dashboard.component';
+import { CustomerDashboardComponent } from './dashboard.component';
 
 
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [CustomerDashboardComponent],
   imports: [
     CommonModule,
   ],
