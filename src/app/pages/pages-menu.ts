edbda2aa8bf4da577f9d26649183367cc9f1a1cc
varshaRaj <minivarsha.a@netcon.in>
2020-11-24 11:51:27 +0530

    authentication changes

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -20,5 +20,26 @@ export const MENU_ITEMS: NbMenuItem[] = [
     title: 'Meetings',
     icon: 'clock-outline',
     link: '/pages/customer/meeting',
+  },{
+    title: 'Auth',
+    icon: 'lock-outline',
+    children: [
+      {
+        title: 'Login',
+        link: '/auth/login',
+      },
+      {
+        title: 'Register',
+        link: '/auth/register',
+      },
+      {
+        title: 'Request Password',
+        link: '/auth/request-password',
+      },
+      {
+        title: 'Reset Password',
+        link: '/auth/reset-password',
+      },
+    ],
   },
 ];
